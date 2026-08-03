e87a6cc1cb500065064951c1460ce829206cc694
yucuxo <junglcuxo@sjtu.edu.cn>
2026-08-03 14:04:33 +0800

    feat(ops): render manual demo seed service

--- backend/src/mneme/ops/deployment.py
+++ backend/src/mneme/ops/deployment.py
@@ -21,6 +21,7 @@ TEMPLATE_FILENAMES = (
     "mneme-ingest.service.template",
     "mneme-ingest.timer.template",
     "mneme-migrate.service.template",
+    "mneme-seed.service.template",
     "mneme-worker.service.template",
     "nginx-mneme-bootstrap.conf.template",
     "nginx-mneme-tls.conf.template",
